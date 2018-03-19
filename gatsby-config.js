18a7e806c3b4611dacc9e871e7ca2940b07b44cc
Nick Lewis <nickeblewis@gmail.com>
2018-03-19 14:39:05 +0000

    first

---
+++ gatsby-config.js
@@ -0,0 +1,83 @@
+const config = require('./data/SiteConfig')
+
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
+module.exports = {
+  pathPrefix: config.pathPrefix,
+  siteMetadata: {
+    siteUrl: config.siteUrl + pathPrefix,
+    rssMetadata: {
+      site_url: config.siteUrl + pathPrefix,
+      feed_url: config.siteUrl + pathPrefix + config.siteRss,
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image_url: `${config.siteUrl + pathPrefix}/logos/logo-512.png`,
+      author: config.userName,
+      copyright: config.copyright
+    }
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        // The base url to your WP site.
+        baseUrl: 'nicklewisweb.wordpress.com',
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: true,
+        // The protocol. This can be http or https.
+        protocol: 'https',
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: false,
+        auth: {
+          // If hostingWPCOM is true then you will need to communicate with wordpress.com API
+          // in order to do that you need to create an app (of type Web) at https://developer.wordpress.com/apps/
+          // then add your clientId, clientSecret, username, and password here
+          wpcom_app_clientSecret:
+            "5NRU148Mrj3QJKL3mptvwO7MGBnMWAuevu1qOIIEdpvcnI2lPoq72QE5saqBxdhb",
+          wpcom_app_clientId: "56577",
+          wpcom_user: "nickeblewis@gmail.com",
+          wpcom_pass: "Winchester72",
+        },
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: false
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: config.themeColor
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-twitter',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/logos/logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logos/logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    },
+    'gatsby-plugin-offline'
+  ]
+}
